f6cdf9fb387df99f4719134843459927bc1c79a7
unknown <986242@CS.MUM.EDU>
2017-12-05 12:23:48 -0600

    Update Datase

--- src/edu/mum/eventmanagement/HibernateUtil.java
+++ src/edu/mum/eventmanagement/HibernateUtil.java
@@ -1,4 +1,8 @@
 package edu.mum.eventmanagement;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
@@ -7,39 +11,25 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	  private static StandardServiceRegistry registry;
 	  private static SessionFactory sessionFactory;
 
 	  public static SessionFactory getSessionFactory() {
-	    if (sessionFactory == null) {
-	      try {
-	        // Create registry
-	        registry = new StandardServiceRegistryBuilder()
-	        		.configure("/resources/hibernate.cfg.xml")
-	            .build();
-
-	        // Create MetadataSources
-	        MetadataSources sources = new MetadataSources(registry);
-
-	        // Create Metadata
-	        Metadata metadata = sources.getMetadataBuilder().build();
-
-	        // Create SessionFactory
-	        sessionFactory = metadata.getSessionFactoryBuilder().build();
-
-	      } catch (Exception e) {
-	        e.printStackTrace();
-	        if (registry != null) {
-	          StandardServiceRegistryBuilder.destroy(registry);
-	        }
-	      }
-	    }
-	    return sessionFactory;
+		  if(sessionFactory == null){
+			  synchronized (HibernateUtil.class) { //Double check if the sessionFactory is null (thread safe)
+				  if(sessionFactory == null){
+					  Configuration configuration = new Configuration()
+							  							.configure("/META-INF/hibernate.cfg.xml");
+					  sessionFactory = configuration
+							  			.buildSessionFactory();
+				  }
+			  }
+		  }
+		  
+		  return sessionFactory;
 	  }
-
-	  public static void shutdown() {
-	    if (registry != null) {
-	      StandardServiceRegistryBuilder.destroy(registry);
-	    }
+	  
+	  public static void updateDatabase() {
+		    EntityManagerFactory factory = Persistence.createEntityManagerFactory("Database"); //name of persistence unit here
+		    EntityManager entityManager = factory.createEntityManager();
 	  }
 }
--- src/edu/mum/eventmanagement/Menu.java
+++ src/edu/mum/eventmanagement/Menu.java
@@ -10,21 +10,20 @@ public class Menu extends WindowBase {
 	}
 	
 	public static void main(String[] args) {
-		//Application.launch(Menu.class, args);
+//Application.launch(Menu.class, args);
 		
 		Session session = HibernateUtil.getSessionFactory().openSession();
 	    session.beginTransaction();
 
 	    // Check database version
-	    String sql = "select version()";
+	    String sql = "select count(*) from information_schema.tables";
 
-	    String result = (String) session.createNativeQuery(sql).getSingleResult();
+	    String result = session.createNativeQuery(sql).getSingleResult().toString();
 	    System.out.println(result);
 
 	    session.getTransaction().commit();
 	    session.close();
-
 	    
-	    HibernateUtil.shutdown();
+	    //HibernateUtil.updateDatabase();
 	}
 }
