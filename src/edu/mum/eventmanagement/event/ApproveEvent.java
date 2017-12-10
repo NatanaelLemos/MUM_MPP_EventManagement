390362d2109302da2c98a5adab5fc9c0661baf4d
DESKTOP-G6VSS41\Truong Dung <truongdung0502@gmail.com>
2017-12-09 22:06:38 -0600

    update missing

--- src/edu/mum/eventmanagement/event/ApproveEvent.java
+++ src/edu/mum/eventmanagement/event/ApproveEvent.java
@@ -4,7 +4,7 @@ package edu.mum.eventmanagement.event;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -25,39 +25,29 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class ApproveEvent implements Initializable {
+public class ApproveEvent {
 	@FXML private TableView<Event> tblEvents;
 	@FXML protected TableColumn<Event, String> colEventName;
 	@FXML protected TableColumn<Event, String> colEventDate;
 	@FXML protected TableColumn<Event, String> colEventDueDate;
 	@FXML protected TableColumn<Event, String> colEventLocation;
 	
-	@FXML private TableView<Schedule> tableViewScheduler;
+	@FXML private TableView<Schedule> tblScheduler;
 	@FXML private TableView<Activity> tableViewActivity;
 	@FXML private TableColumn<Event, String> name;
 	private Event selectedEvent;
 	ObservableList<Event> data;
 	
-	@FXML protected TableColumn<edu.mum.eventmanagement.models.Schedule, String> colTimeStart;
-	@FXML protected TableColumn<edu.mum.eventmanagement.models.Schedule, String> colTtimeEnd;
-	@FXML protected TableColumn<edu.mum.eventmanagement.models.Schedule, String> colActivityName;
+	@FXML protected TableColumn<Schedule, String> colTimeStart;
+	@FXML protected TableColumn<Schedule, String> colTimeEnd;
 
-    //@FXML private TableColumn<Event, EventState> State;
 	
 	private DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
 	    
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+	public void initialize() {
 		// TODO Auto-generated method stub		
-		
-//		 data = tblEvents.getItems();
-//
-//		EventRepository er = new EventRepository();
-//		List <Event> events = er.getAll();
-//		for(Event e : events) {
-//			data.add(new Event(e.getName(), e.getDate(), e.getDueDate(), e.getLocation()));	
-//		}
-		
+		displayTblAdvertisements(false);
+			
 		WindowUtils.setDateColumn(colEventDate);
 		WindowUtils.setDateColumn(colEventDueDate);
         
@@ -70,57 +60,29 @@ public class ApproveEvent implements Initializable {
         List<Event> events = er.getAll();
 		tblEvents.getItems().setAll(events);
 
-//		for(Schedule s : Schedules) {
-//			tableViewScheduler.getItems().add(new Schedule(s.getTimeStart(), s.getTimeEnd()));
-//		}
-		
-		//ActivityRepository ar = new ActivityRepository();
-		//ar.getAll();
-		
-		//ScheduleRepository sr = new ScheduleRepository();
-		//sr.getAll();
-		
-		
-		//tableView.getSelectionModel().selectedIndexProperty().addListener(
-		//		new RowSelectChangeListener());
-		
 		tblEvents.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
 			selectedEvent = newSelection;
 			loadSchedule(newSelection);
 		});
 	}
 	
+	private void displayTblAdvertisements(boolean isVisible) {
+		tblScheduler.setVisible(isVisible);
+	}
+	
 	private void loadSchedule(Event ev) {
 		
-		
+		colTimeStart.setCellValueFactory(new PropertyValueFactory<Schedule, String>("timeStart"));
+		colTimeEnd.setCellValueFactory(new PropertyValueFactory<Schedule, String>("timeEnd"));
 
-//		colTimeStart.setCellValueFactory(new PropertyValueFactory<edu.mum.eventmanagement.models.Schedule, String>("timeStart"));
-//		colTimeStart.setCellValueFactory(new PropertyValueFactory<edu.mum.eventmanagement.models.Schedule, String>("timeEnd"));
-//
 		List<edu.mum.eventmanagement.models.Schedule> ss = ev.getSchedules();
 		System.out.println(ss.size());
-//		tableViewScheduler.getItems().setAll(ss);
+		tblScheduler.getItems().setAll(ss);
+		
+		displayTblAdvertisements(true);
 	}
 	
-	private class RowSelectChangeListener implements ChangeListener<Number> {
 
-		@Override
-		public void changed(ObservableValue<? extends Number> ov, 
-				Number oldVal, Number newVal) {
-
-			int ix = newVal.intValue();
-			
-			//System.out.print(ix);
-
-			if ((ix < 0) || (ix >= data.size())) {
-	
-				return; // invalid data
-			}
-
-			Event ev = data.get(ix);
-			System.out.println(ev.getName());
-		}
-	}
 	
 	
 	
